c75a7353f2330b73b3a4c18a73ed3bc0e9c49f74
Joseph Hayes <joey.hayes.1996@gmail.com>
2022-11-17 18:26:07 -0700

    Began adding CSS, improved HTML and JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -39,11 +39,71 @@ const questions = [
     {
         Q: "Is Java different from JavaScript?",
         1: "Yes",
-        2: "No"
+        2: "No",
         3: "They're the same thing",
-        answer: 0
+        answer: 1
     },
     {
-        Q: "..."
+        Q: "Which of the following is currently NOT a JavaScript framework?",
+        1: "Angular",
+        2: "React",
+        3: "Koffi",
+        answer: 3
+    },
+    {
+        Q: "Inside of which HTML element do we put the JavaScript?",
+        1: "<scripting>",
+        2: "<script>",
+        3: "<javascript>",
+        answer: 2
+    },
+    {
+        Q: "How do you alert Hello World to a page?",
+        1: "alertBox('Hello World');",
+        2: "alert('Hello World');",
+        3: "msgbox('Hello World');",
+        answer: 2
+    },
+    {
+        Q: "How do you call a function named 'myFunction'?",
+        1: "call function myFunction()",
+        2: "call myFunction",
+        3: "myFunction()",
+        answer: 3
+    },
+    {
+        Q: "What is the correct syntax for an IF statement?",
+        1: "if (i === 5)",
+        2: "if i === 5 then",
+        3: "if i === 5",
+        answer: 1
+    },
+    {
+        Q: "What's the correct syntax to start a FOR loop?",
+        1: "for (i < 5; i++)",
+        2: "for let i = 1 to 5; i++",
+        3: "for (let i = 0; i < 5; i++)",
+        answer: 3
+    },
+    {
+        Q: "How do you add a comment in JavaScript?",
+        1: "//Comment",
+        2: "<!--Comment-->",
+        3: "*Comment",
+        answer: 1
+    },
+    {
+        Q: "How do you add a multi-line comment in JavaScript?",
+        1: "<!--Comment-->",
+        2: "/* Comment */",
+        3: "//Comment //",
+        answer: 2
+    },
+    {
+        Q: "How do you declare a variable as an array?",
+        1: "let array = array",
+        2: "let array = 'array[5]'",
+        3: "let array = []",
+        answer: 3
     }
 ]
